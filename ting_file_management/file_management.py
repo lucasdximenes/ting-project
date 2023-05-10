fc15adc1422b71075d4831139a389a666afe776a
Lucas Ximenes <lucasdximenes@gmail.com>
2023-05-10 18:09:09 -0300

    feat: :rocket: implement `txt_importer` function

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    # Verifica se o arquivo existe
+    if not os.path.exists(path_file):
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+    # Verifica se a extensão do arquivo é .txt
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write("Formato inválido\n")
+
+    # Importa as linhas do arquivo e retorna em uma lista
+    with open(path_file, 'r') as file:
+        lines = file.read().split('\n')
+    return lines
