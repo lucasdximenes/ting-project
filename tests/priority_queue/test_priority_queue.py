93309fc4c0b32b28168cace0c619977f3901692a
Lucas Ximenes <lucasdximenes@gmail.com>
2023-05-10 20:50:20 -0300

    test: :white_check_mark: implement `PriorityQueue` test

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,18 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+    priority_queue.enqueue({"qtd_linhas": 1})
+    priority_queue.enqueue({"qtd_linhas": 10})
+    priority_queue.enqueue({"qtd_linhas": 5})
+
+    assert len(priority_queue) == 3
+    assert priority_queue.dequeue() == {"qtd_linhas": 1}
+
+    priority_queue.enqueue({"qtd_linhas": 2})
+    assert priority_queue.search(0) == {"qtd_linhas": 2}
+
+    with pytest.raises(IndexError):
+        priority_queue.search(3)
