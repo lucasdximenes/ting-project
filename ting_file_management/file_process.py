9bceb6d2519a67511b36bd21518feefa54cfabbd
Lucas Ximenes <lucasdximenes@gmail.com>
2023-05-10 19:54:53 -0300

    feat: :rocket: implement `process` function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from .file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    Queue = instance
+    file_lines = txt_importer(path_file)
+
+    if any(path_file == data['nome_do_arquivo'] for data in Queue.queue):
+        return
+
+    dict_data = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(file_lines),
+        'linhas_do_arquivo': file_lines,
+    }
+
+    Queue.enqueue(dict_data)
+    sys.stdout.write(str(dict_data))
 
 
 def remove(instance):
