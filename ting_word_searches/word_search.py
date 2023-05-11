a7d3ec0ef95e5daf0b9b2b7f4aaebb64f22e684d
Lucas Ximenes <lucasdximenes@gmail.com>
2023-05-10 21:02:08 -0300

    feat: :rocket: implement `exists_word` function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,29 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
 
 
-def search_by_word(word, instance):
+def exists_word(word, instance: Queue):
+    Queue = instance
+    list_word_occurrences = []
+    for i in range(len(Queue.queue)):
+        data = Queue.search(i)
+        occurrences = []
+        for j in range(len(data['linhas_do_arquivo'])):
+            if word.lower() in data['linhas_do_arquivo'][j].lower():
+                occurrences.append(
+                    {
+                        "linha": j + 1,
+                    }
+                )
+        if len(occurrences) > 0:
+            list_word_occurrences.append(
+                {
+                    "palavra": word,
+                    "arquivo": data['nome_do_arquivo'],
+                    "ocorrencias": occurrences,
+                }
+            )
+    return list_word_occurrences
+
+
+def search_by_word(word, instance: Queue):
     """Aqui irá sua implementação"""
